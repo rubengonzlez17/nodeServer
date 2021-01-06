21d013039a7a6ce58d6dfbdf29f68f11e79369e5
ruben <rubengonzlez17@usal.es>
2021-01-06 23:27:20 +0100

    fix Rounds

--- src/controllers/playersController.ts
+++ src/controllers/playersController.ts
@@ -30,7 +30,7 @@ enum strategies {
 
 export class PlayersController{
     public api_address: string = process.env.MANAGER_API + "/" + endpoints.User.toLowerCase();
-    public allRounds = ["R1","R2","R3","R4","R5","R6","R7","R8","R9","R10","R11","R12","R13","R14","R15","R16","R15","R16","R15","R16","R15","R16","R17","R18","R19","R20","R21","R22","R23","R24","R25","R26","R27","R28","R29","R30","R31","R32","R33","R34","R35","R36","R37","R38"];
+    public allRounds = ["R1","R2","R3","R4","R5","R6","R7","R8","R9","R10","R11","R12","R13","R14","R15","R16","R17","R18","R19","R20","R21","R22","R23","R24","R25","R26","R27","R28","R29","R30","R31","R32","R33","R34","R35","R36","R37","R38"];
     public listStrategy = ["4-3-3", "3-4-3", "3-5-2", "4-4-2","4-5-1","5-3-2", "5-4-1"]
     
     /**
